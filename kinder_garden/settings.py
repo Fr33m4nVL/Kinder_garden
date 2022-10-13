e0cb166d5a33880b5b4b8cac81a72af27006cb7f
Fr33m4nVL <vladislavtrapezjikov@gmail.com>
2022-10-13 19:05:19 +0500

    staticfiels

--- kinder_garden/settings.py
+++ kinder_garden/settings.py
@@ -26,9 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
-    #'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'widget_tweaks',
-    'corsheaders',
+    #'corsheaders',
     'users.apps.UsersConfig',
     'news.apps.NewsConfig',
 ]
