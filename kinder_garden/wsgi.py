3341b152f08911fbdfb953ef8b3bb8562aa33926
Fr33m4nVL <vladislavtrapezjikov@gmail.com>
2022-10-13 20:52:06 +0500

    upd wsgi

--- kinder_garden/wsgi.py
+++ kinder_garden/wsgi.py
@@ -1,7 +1,10 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kinder_garden.settings')
 
-application = get_wsgi_application()
+application = WhiteNoise(get_wsgi_application())
+
+#application = get_wsgi_application()
