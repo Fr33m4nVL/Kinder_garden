2e41ce56a3e11ffeaa0f9c2cecf29c7ebcdcbaac
Fr33m4nVL <vladislavtrapezjikov@gmail.com>
2022-11-16 15:41:29 +0500

    fuck you images

--- news/forms.py
+++ news/forms.py
@@ -1,18 +1,9 @@
 from django import forms
-from multiupload.fields import MultiImageField
 
 from .models import Post
 
-class UploadNewsForm(forms.ModelForm):
+class PostForm(forms.ModelForm):
+    images = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
     class Meta:
         model = Post
-        fields = ['title', 'body', 'preview', 'author']
-
-    images = MultiImageField(max_num=20)
-
-    def save(self, commit=True):
-        instance = super(UploadNewsForm, self).save(commit)
-        for each in self.cleaned_data['images']:
-            Post.objects.create(image=each, post=instance)
-
-        return instance
+        fields = ['title', 'body', 'preview', 'images', 'author']
--- news/views.py
+++ news/views.py
@@ -1,9 +1,10 @@
-from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Post
-from .forms import UploadNewsForm
+from .models import Post, Image
+from .forms import PostForm
 
 class NewsListView(ListView):
     model = Post
@@ -14,22 +15,34 @@ class NewsDetailView(DetailView):
     template_name = 'post_detail.html'
 
 class NewsCreateView(CreateView):
-    #model = Post
-    form_class = UploadNewsForm
+    model = Post
+    form_class = PostForm
     template_name = 'post_new.html'
-    #success_url = reverse('home')
-    #fields = ['title', 'body', 'preview', 'image', 'author']
 
-    def get_absolute_url(self):
-        return reverse('post_detail', args=[str(self.id)])
+    def post(self, request):
+        if request.method == 'POST':
+            post = Post.objects.create(
+                title=request.POST['title'],
+                body=request.POST['body'],
+                preview=request.FILES['preview'],
+                author=request.user
+            )
+            images = request.FILES.getlist('images')
+            print(images)
+
+            for f in images:
+                a = Image(post=post, image=f)
+                a.save()
+            #return reverse('post_detail', args=[str(post.pk)])
+            return HttpResponseRedirect('/')
 
 
-class NewsEditView(UpdateView):
-    model = Post
-    template_name = 'post_edit.html'
-    fields = ['title', 'body', 'preview', 'pictures']
+#class NewsEditView(UpdateView):
+    #model = Post
+    #template_name = 'post_edit.html'
+    #fields = ['title', 'body', 'preview', 'pictures']
 
-class DeleteView(DeleteView):
-    model = Post
-    template_name = 'post_delete.html'
-    success_url = reverse_lazy('home')
+#class DeleteView(DeleteView):
+    #model = Post
+    #template_name = 'post_delete.html'
+    #success_url = reverse_lazy('home')
